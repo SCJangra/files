a71104d5343a42ea310139a9c1e927b476bc146f
Sachin Charakhwal <sachincharakhwal@gmail.com>
2022-01-19 18:16:07 +0530

    feat: expose rpc method `list` to list metadata of files in a directory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use rpc::Rpc;
+use jsonrpc_core as jrpc;
+use jsonrpc_ipc_server as ipc;
+use jsonrpc_pubsub as ps;
+
+mod rpc;
+
 fn main() {
-    println!("Hello, world!");
+    let mut io = jrpc::MetaIoHandler::default();
+    let rpc = rpc::RpcImpl;
+    io.extend_with(rpc.to_delegate());
+
+    let builder = ipc::ServerBuilder::with_meta_extractor(io, |request: &ipc::RequestContext| {
+        std::sync::Arc::new(ps::Session::new(request.sender.clone()))
+    });
+    let server = builder.start("/tmp/files").expect("Couldn't open socket");
+    server.wait();
 }
---
+++ src/rpc.rs
@@ -0,0 +1,70 @@
+use files::{file, task as rpc_task};
+use futures::TryFutureExt;
+use jsonrpc_core as jrpc;
+use jsonrpc_pubsub::{self as ps, typed as pst};
+use ps::manager::IdProvider;
+use tokio::task;
+
+lazy_static::lazy_static! {
+    static ref RAND_STR_ID: ps::manager::RandomStringIdProvider =
+        ps::manager::RandomStringIdProvider::new();
+}
+
+#[jsonrpc_derive::rpc(server)]
+pub trait Rpc {
+    type Metadata;
+
+    #[pubsub(subscription = "list", subscribe, name = "list")]
+    fn list(&self, m: Self::Metadata, sub: pst::Subscriber<rpc_task::TaskResult>, id: file::FileId);
+
+    #[pubsub(subscription = "list", unsubscribe, name = "cancel_list")]
+    fn cancel_list(
+        &self,
+        m: Option<Self::Metadata>,
+        id: ps::SubscriptionId,
+    ) -> jrpc::BoxFuture<jrpc::Result<bool>>;
+}
+
+pub struct RpcImpl;
+
+impl RpcImpl {
+    async fn get_sink(
+        sub: pst::Subscriber<rpc_task::TaskResult>,
+    ) -> anyhow::Result<(ps::SubscriptionId, pst::Sink<rpc_task::TaskResult>)> {
+        let id = ps::SubscriptionId::String(RAND_STR_ID.next_id());
+        let sink = sub
+            .assign_id_async(id.clone())
+            .await
+            .map_err(|_| anyhow::anyhow!("Could not get a sink! Request already terminated!"))?;
+        Ok((id, sink))
+    }
+}
+
+impl Rpc for RpcImpl {
+    type Metadata = std::sync::Arc<ps::Session>;
+
+    fn list(
+        &self,
+        _m: Self::Metadata,
+        sub: pst::Subscriber<rpc_task::TaskResult>,
+        id: file::FileId,
+    ) {
+        task::spawn(async move {
+            let (task_id, sink) = Self::get_sink(sub)
+                .inspect_err(|_e| { /* TODO: Log this error */ })
+                .await?;
+
+            rpc_task::list(id, task_id, sink).await;
+
+            Ok::<(), anyhow::Error>(())
+        });
+    }
+
+    fn cancel_list(
+        &self,
+        _m: Option<Self::Metadata>,
+        id: ps::SubscriptionId,
+    ) -> jrpc::BoxFuture<jrpc::Result<bool>> {
+        Box::pin(rpc_task::cancel_task(id))
+    }
+}
