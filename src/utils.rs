c710c79381f2d6e05247f642c4f77b6fe248e3e8
Sachin Charakhwal <sachincharakhwal@gmail.com>
2022-01-23 12:13:53 +0530

    dev: add macros to simplify sending jrpc notifications

--- src/utils.rs
+++ src/utils.rs
@@ -7,3 +7,21 @@ pub fn to_rpc_err(err: anyhow::Error) -> jrpc::Error {
         data: Some(json::json!(err.root_cause().to_string())),
     }
 }
+
+#[macro_export]
+macro_rules! notify_err {
+    ($sink: ident, $err: expr) => {{
+        $sink
+            .notify(Err($err))
+            .map_err(|e| anyhow::anyhow!("Could not notify error '{}'", e))
+    }};
+}
+
+#[macro_export]
+macro_rules! notify_ok {
+    ($sink: ident, $val: expr) => {{
+        $sink
+            .notify(Ok($val))
+            .map_err(|e| anyhow::anyhow!("Could not notify result '{}'", e))
+    }};
+}
